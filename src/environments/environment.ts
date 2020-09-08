44dc76b0f9ca5cda1fba4cefb6d7774d49533205
Jose Bocanegra <j.bocanegra@uniandes.edu.co>
2020-09-08 07:34:00 -0500

    Add integration test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-//const host = '157.253.238.75';
-//const port = '8084';
-const host = 'localhost';
-const port = '8080';
-const appName = 'frontstepbystep-api';
-const rootApi = 'api';
+const host = "157.253.238.75";
+const port = "8084";
+//const host = 'localhost';
+//const port = '8080';
+const appName = "frontstepbystep-api";
+const rootApi = "api";
 
 export const environment = {
   production: false,
-  baseUrl: `http://${host}:${port}/${appName}/${rootApi}/`
+  baseUrl: `http://${host}:${port}/${appName}/${rootApi}/`,
 };
-
